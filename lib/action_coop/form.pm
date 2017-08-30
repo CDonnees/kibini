24cc9fa0677364d289b4bab8583c1425d9e7c6c2
ahrbx <ragbx@ovh.fr>
2017-08-30 14:17:30 +0200

    action_coop

--- lib/action_coop/form.pm
+++ lib/action_coop/form.pm
@@ -13,23 +13,23 @@ use kibini::db ;
 
 sub GetListActionsCooperation {
     my $dbh = GetDbh() ;
-	my $req = <<SQL;
+    my $req = <<SQL;
 SELECT
 *
-FROM kib_sandbox.stat_action_coop
+FROM statdb.stat_action_coop
 ORDER BY idstat_action_coop DESC
 SQL
-	return GetAllArrayRef($req) ;
+    return GetAllArrayRef($req) ;
 }
 
 sub AddActionCooperation {
-	my ( $date, $lieu, $type, $nom, $type_structure, $nom_structure, $participants, $referent_action ) = @_ ;
-	my $dbh = GetDbh() ;
-	my $req = "INSERT INTO kib_sandbox.stat_action_coop (date, lieu, type, nom, type_structure, nom_structure, participants, referent_action ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)" ;
-	my $sth = $dbh->prepare($req);
-	$sth->execute( $date, $lieu, $type, $nom, $type_structure, $nom_structure, $participants, $referent_action ) ;
-	$sth->finish();
-	$dbh->disconnect();
+    my ( $date, $lieu, $type, $nom, $type_structure, $nom_structure, $participants, $referent_action ) = @_ ;
+    my $dbh = GetDbh() ;
+    my $req = "INSERT INTO kib_sandbox.stat_action_coop (date, lieu, type, nom, type_structure, nom_structure, participants, referent_action ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)" ;
+    my $sth = $dbh->prepare($req);
+    $sth->execute( $date, $lieu, $type, $nom, $type_structure, $nom_structure, $participants, $referent_action ) ;
+    $sth->finish();
+    $dbh->disconnect();
 }
 
 1;
